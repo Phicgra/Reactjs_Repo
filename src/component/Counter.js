790c31d828a87e5f3247f802b201b873dac6d70d
phicgra <emmanueligeng01@gmail.com>
2023-11-30 14:36:12 +0100

    modified version for the counter

--- src/component/Counter.js
+++ src/component/Counter.js
@@ -2,6 +2,11 @@ import React, { useState } from 'react'
 
 export const Counter = () => {
     let [nums, setNums] = useState(0);
+    
+    let  reSet =() => {
+        setNums(nums = 0)
+    }
+
     let handlClick = () => {
         setNums (nums +1);
     }
@@ -12,6 +17,7 @@ export const Counter = () => {
         <p>Here, we increase a paticular given number by 1 <b>( i.e given number ++1 )</b></p>
         <p>Count -- {nums}</p>
         <button onClick={handlClick}>Increment</button>
+        <button onClick={reSet}>Reset</button>
 
     </div>
   )
