94766c3357f61f5a141666e020f47479444e2bac
phicgra <emmanueligeng01@gmail.com>
2023-11-30 13:03:07 +0100

    modified  the changes in message.js file

--- src/component/Message.js
+++ src/component/Message.js
@@ -4,19 +4,21 @@ import { useState } from 'react';
 export const Message = () => {
   // Declare state variable 'name' and its updater function 'setName'
   let [name, setName] = useState('Emmanuel');
+  let [age, setAge] = useState(20)
 
   // Define a function to handle the button click and update the 'name' state
   const handleClick = () => setName('Asima');
+  
 
   // JSX content to render
   return (
     <div>
       <h2>
-        Welcome {name}!
+        Welcome {name}! <br/> {name} is {age} years old
       </h2>
       <button onClick={handleClick}>Subscribe</button>
     </div>
   );
 };
-
+ 
 export default Message;
